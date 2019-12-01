55ac3c6f75e87d181674d6eec53accd9ad574f1d
Alex Gustafsson <alex-gustafsson@live.se>
2019-12-01 21:36:47 +0100

    Added mock cards

--- src/pages/home/model.vue
+++ src/pages/home/model.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="home-page">
-    <h1>{{greeting}}</h1>
+    <main>
+      <div v-for="timekeep in timekeepings" class="card">
+        <header>
+          <div class="inset">
+            <b>{{timekeep.name[0]}}</b>
+          </div>
+          <h1>{{timekeep.name}}</h1>
+        </header>
+        <h2>{{timekeep.time}}</h2>
+        <footer>
+          <p>{{timekeep.previous}}</p>
+        </footer>
+      </div>
+    </main>
   </div>
 </template>
 
--- src/pages/home/script.js
+++ src/pages/home/script.js
@@ -2,7 +2,26 @@ export default {
   name: 'home-page',
   data() {
     return {
-      greeting: 'Hello World!'
+      timekeepings: [
+        {
+          id: 0,
+          name: 'Skanska',
+          time: 12345,
+          previous: 45678
+        },
+        {
+          id: 1,
+          name: 'Stora Enso',
+          time: 12345,
+          previous: 456
+        },
+        {
+          id: 2,
+          name: 'SAAB',
+          time: 12345,
+          previous: 45
+        }
+      ]
     };
   }
 };
