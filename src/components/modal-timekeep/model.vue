2051e866e40f61fb6c855510da89f41b8a49be2d
Alex Gustafsson <alex-gustafsson@live.se>
2020-01-21 11:51:19 +0100

    Added close button to timekeep modal

--- src/components/modal-timekeep/model.vue
+++ src/components/modal-timekeep/model.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="modal-timekeep">
     <form-input name="name" type="text" label="Name" v-model="timekeep.name"></form-input>
+    <form-input name="submit" type="button" label="Close" @click="close()"></form-input>
   </div>
 </template>
 
 <script src="./script.js"></script>
+<style src="./style.css"></style>
--- src/components/modal-timekeep/script.js
+++ src/components/modal-timekeep/script.js
@@ -2,6 +2,11 @@ import {default as FormInput} from '../form-input/model.vue';
 
 export default {
   name: 'modal-timekeep',
+  methods: {
+    close() {
+      this.$modal.close();
+    }
+  },
   props: {
     timekeep: {
       type: Object,
