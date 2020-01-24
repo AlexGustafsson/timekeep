b427a420117a448b8f10b2cafa9e612042c30408
Alex Gustafsson <alex-gustafsson@live.se>
2020-01-24 10:50:44 +0100

    Updated about modal

--- src/components/modal-about/model.vue
+++ src/components/modal-about/model.vue
@@ -3,7 +3,7 @@
     <h1>Timekeep 1.0.0</h1>
     <p>Timekeep is a simple timekeeping web app. It's intended to be easy to use.</p>
     <p>It is currently considered an alpha-level application and as such it should not be used as the sole way of tracking time.</p>
-    <p>The application is currently entirely free to use and open source over at <a href="https://github.com/AlexGustafsson/timekeep">https://github.com/AlexGustafsson/timekeep</a></p>
+    <p>The application is entirely free to use and open source over at <a href="https://github.com/AlexGustafsson/timekeep">https://github.com/AlexGustafsson/timekeep</a></p>
   </div>
 </template>
 
