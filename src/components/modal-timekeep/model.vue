42da3eba91cf9482958ad4050a915099809feb5a
Alex Gustafsson <alex-gustafsson@live.se>
2021-03-30 21:41:09 +0200

    Remove old components

--- src/components/modal-timekeep/model.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="modal-timekeep">
-    <form-input name="name" type="text" label="Name" v-model="timekeep.name"></form-input>
-    <form-input name="submit" type="button" label="Close" @click="close()"></form-input>
-  </div>
-</template>
-
-<script src="./script.js"></script>
-<style src="./style.css"></style>
