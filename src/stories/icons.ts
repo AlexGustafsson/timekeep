6f80b682ac2700eb21acde8ef094731e75b4843a
Alex Gustafsson <alex-gustafsson@live.se>
2021-04-03 17:21:34 +0200

    Fix icon storyboard

--- src/stories/icons.ts
+++ src/stories/icons.ts
@@ -1,20 +1,23 @@
-import {DefineComponent} from "vue";
 import IonAdd from '../components/ion-icons/add.vue';
 import IonFavorite from '../components/ion-icons/favorite.vue';
 import IonMore from '../components/ion-icons/more.vue';
 import IonStopwatch from '../components/ion-icons/stopwatch.vue';
 
 export default {
-  title: 'Icons',
-  component: IonAdd
+  title: 'Icons'
 };
 
-const Template = (component: DefineComponent, name: string) => ({
-  components: { IonAdd },
-  template: `<${name} />`,
-  setup() {
-    return {};
-  }
-});
+const Template = (component: any, name: string) => {
+  return () => ({
+    components: { [name]: component },
+    template: `<${name} />`,
+    setup() {
+      return {};
+    }
+  });
+}
 
 export const Add = Template(IonAdd, 'ion-add');
+export const Favorite = Template(IonFavorite, 'ion-favorite');
+export const More = Template(IonMore, 'ion-more');
+export const Stopwatch = Template(IonStopwatch, 'ion-stopwatch');
