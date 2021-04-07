f9298072af90573c1c0f4348ba2af5dad5bd8bd3
Alex Gustafsson <alex-gustafsson@live.se>
2021-04-07 19:34:09 +0200

    Fix linting of Vue 3

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/typescript/recommended", "@vue/prettier", "@vue/prettier/@typescript-eslint"],
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/typescript/recommended", "@vue/prettier", "@vue/prettier/@typescript-eslint"],
   parserOptions: {
     ecmaVersion: 2020,
   },
